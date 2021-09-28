597f5344942599398a45abaab9a3422fea739f86
pakasit.san <pakasit.bkk@gmail.com>
2021-09-28 20:21:52 +0700

    Add mysql connector dependency

--- src/main/java/com/example/springbootcrud/entities/UserEntity.java
+++ src/main/java/com/example/springbootcrud/entities/UserEntity.java
@@ -8,9 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
 @Getter
 @Setter
+@Entity(name = "User")
 public class UserEntity {
 
     @Id
